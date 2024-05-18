0b1db49c873f11b4bd873336c145f611f04dd428
Sithija Lakshan <sithikaru@gmail.com>
2024-05-18 12:57:28 +0530

    navbar done

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import command from '../assets/command.svg';
 import { faBell, faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => {
   return (
@@ -29,11 +30,12 @@ const Navbar = () => {
             /> F
           </span>
         </div>
-        <div className="flex items-center space-x-2">
-            <FontAwesomeIcon icon={faEnvelope} className="mr-3 w-5 rounded-full hover:text-[#6891f1] h-5 my-auto" />
+        <div className="flex items-center space-x-5">
+            <FontAwesomeIcon icon={faEnvelope} className="mr-3 ml-5 w-5 rounded-full hover:text-[#6891f1] h-5 my-auto" />
             <FontAwesomeIcon icon={faBell} className="mr-3 w-5 rounded-full hover:text-[#6891f1] h-5 my-auto" />
-            <div>
-              
+            <div className='flex'>
+              <img src='https://i.pinimg.com/originals/3e/2e/8c/3e2e8c6fa626636eb4e8bdfe78edab3b.jpg' className='w-6 h-6 rounded-full'/>
+              <FontAwesomeIcon icon={faChevronDown} className="mr-3 w-3 rounded-full text-[#acb8c8] ml-1 hover:text-[#6891f1] h-3 my-auto" />
             </div>
         </div>
       </div>
